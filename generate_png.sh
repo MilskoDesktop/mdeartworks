9418e591099e3917cac5141ecec5023194455a4d
NishiOwO <nishi@nishi.boats>
2025-11-23 02:34:40 +0900

    aaa

--- generate_png.sh
+++ generate_png.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cd icons/scalable
 for i in 128 64 32 16; do
+	echo "-- ${i}x${i}"
 	for j in *.svg; do
+		echo "$j"
 		rsvg-convert --height=$i -o ../${i}x${i}/`echo $j | cut -d. -f1`.png $j
 	done
 done
